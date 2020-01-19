6cb603297f780cffac04a1474eb7553252895ec4
Yiling Liu <lyleaf@gmail.com>
2020-01-19 15:14:14 +1100

    add random filename generator

--- wav_recorder.py
+++ wav_recorder.py
@@ -4,20 +4,24 @@ from pyo import *
 import librosa
 import numpy as np
 import librosa.display
+import uuid
 
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 1
 fs = 44100  # Record at 44100 samples per second
-seconds = 10
-filename = "output.wav"
+seconds = 1
+
+random_uuid = str(uuid.uuid4())
+filename = "niubi%s.wav" % random_uuid
 chunk = 2048 #fs*5  # Record in chunks of 1024 samples
 
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
 
-device = 'Pixel USB-C earbuds'
+device = 'MacBook Pro Microphone'
 dev_list, dev_index =  pa_get_input_devices()
+print(dev_list)
 # ['Pixel USB-C earbuds', 'MacBook Pro Microphone']
 dev = dev_index[dev_list.index(device)]
 
@@ -55,28 +59,3 @@ wf.setframerate(fs)
 wf.writeframes(b''.join(frames))
 wf.close()
 print('Finished recording')
-
-
-# Understand tempo
-y, sr = librosa.load('output.wav')
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo/2))
-onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
-
-o_env = librosa.onset.onset_strength(y, sr=sr)
-times = librosa.times_like(o_env, sr=sr)
-
-import matplotlib.pyplot as plt
-D = np.abs(librosa.stft(y))
-plt.figure()
-ax1 = plt.subplot(2, 1, 1)
-librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
-                          x_axis='time', y_axis='log')
-plt.title('Power spectrogram')
-plt.subplot(2, 1, 2, sharex=ax1)
-plt.plot(times, o_env, label='Onset strength')
-plt.vlines(times[onset_frames], 0, o_env.max(), color='r', alpha=0.9,
-            linestyle='--', label='Onsets')
-plt.axis('tight')
-plt.legend(frameon=True, framealpha=0.75)
-plt.show()
